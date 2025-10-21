d046a740acfa6ce830b02c0fa8b104043f692761
AshifurNahid <ashifur.rahman@sonaliintellect.com>
2025-10-21 17:48:57 +0600

    update

---
+++ service/audit/src/main/java/com/nahid/audit/entity/AuditLog.java
@@ -0,0 +1,39 @@
+package com.nahid.audit.entity;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Map;
+
+@Data
+@Table(name="audit_log")
+@Entity
+public class AuditLog {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String eventId;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String eventType;
+
+    @Column(name="entity_name", nullable = false, length = 100)
+    private String entityName;
+
+    @Column(name="entity_id")
+    private String entityId;
+
+    private String actionType;
+
+//    @Type(type = "jsonb")
+//    @Column(columnDefinition = "jsonb")
+//    private Map<String, Object> beforeState;
+//
+//    @Type(type = "jsonb")
+//    @Column(columnDefinition = "jsonb")
+//    private Map<String, Object> afterState;
+
+}
