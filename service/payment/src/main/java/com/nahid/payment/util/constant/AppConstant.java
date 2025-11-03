83ed607f553c592de1fa354a81082fb954748815
Ashifur Rahman <97047681+AshifurNahid@users.noreply.github.com>
2025-11-03 22:50:39 +0600

    feat(payment): add custom api response builder

--- service/payment/src/main/java/com/nahid/payment/util/constant/AppConstant.java
+++ service/payment/src/main/java/com/nahid/payment/util/constant/AppConstant.java
@@ -6,4 +6,11 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class AppConstant {
     public static final String PAYMENT = "Payment";
+    public static final String PAYMENTS = "Payments";
+    public static final String USER_PAYMENTS = "User payments";
+    public static final String RECENT_PAYMENTS = "Recent payments";
+    public static final String TOTAL_PAID_AMOUNT = "Total paid amount";
+    public static final String CANCELLED = "cancelled";
+    public static final String REFUNDED = "refunded";
+    public static final String RETRIED = "retried";
 }
